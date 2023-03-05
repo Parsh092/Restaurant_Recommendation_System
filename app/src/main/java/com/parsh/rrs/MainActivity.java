03a2dd2cd0c56cc8f43aad0e72e89daaf3106320
Ritesh Yadav <riteshyadav070802@gmail.com>
2023-03-05 15:41:37 +0530

    by ritesh

--- app/src/main/java/com/parsh/rrs/MainActivity.java
+++ app/src/main/java/com/parsh/rrs/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //commit by ritesh
         //commit by himanshu shahu..
+        //okay
     }
 }
